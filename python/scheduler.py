4c5ab9e7d9152d2711a66a62725e123a4987b2b9
D1ng0 <diego@Diegos-MacBook-Pro.local>
2017-09-18 17:00:09 -0700

    Correlation Table done.

--- python/scheduler.py
+++ python/scheduler.py
@@ -3,9 +3,17 @@ import json, csv, os, threading, sched, time
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import numpy as np
+import seaborn as sns
 volumes = []
 prices = []
+# graph type
+plt.style.use('ggplot')
+# set pandas stdout options
+pd.set_option('display.height', 1000)
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 
 def new_markets(daysback=120):
 	bittrex = Bittrex(None, None)
@@ -113,11 +121,72 @@ def get_market_data(market, labels):
 	summary = market_summary['result']
 	# print(market)
 	data = {}
-	data['Market'] = market
+	data['Marnbket'] = market
 	for key in labels:
 		data[key] = summary[0][key]
 	return data
 
+def markets_correlation():
+	
+	x = 300 # limit of markets to test
+	vol_threshold = 30000000
+
+	marketsList = get_markets()
+	main_df = pd.DataFrame() # empty df
+	corr_threshold = 0.8
+
+	for market in marketsList[:x]: #traverse the markets 
+		filename = market+'.csv'
+		path = os.path.join('data', filename)
+		df = pd.read_csv(path,
+							header=0, 
+							usecols=['TimeStamp','Last','Volume']) # read csv with timestamp-object as index doing a partial loading
+		df['TimeStamp'] = pd.to_datetime(df['TimeStamp'])
+		df['TimeStamp'] = df['TimeStamp'].values.astype('<M8[h]') # recalculate timestamp
+		volume_mean = df['Volume'].mean() # find volume mean
+		df = df.drop('Volume',axis=1) # drop the volume column
+		df = df.drop_duplicates(subset='TimeStamp', keep='last') # remove duplicates
+		df.set_index('TimeStamp', inplace=True)
+		df.rename(columns={'Last':market},inplace=True) # rename the colum with the pair name
+
+		if volume_mean >= vol_threshold:
+			if main_df.empty: # join df
+				main_df = df
+			else:
+				main_df = main_df.join(df)
+		else:
+			main_df = main_df
+		main_df.fillna(method='ffill', inplace=True) # fill NANs
+
+	# print(main_df.tail(20))
+	if not main_df.empty:
+		corr_df = main_df.corr()
+		# print(corr_df)
+		for row in corr_df.iterrows():
+			index, data = row
+			# print(data.tolist())
+			drop_row = True
+			for item in data.tolist():
+				if item < 1 and item >= corr_threshold:
+					# print(item)
+					drop_row = False # there is a candidate value
+				# else:
+					# corr_df = corr_df.drop(corr_df[index])
+					# df.drop(df.index[[1,3]], inplace=True)
+					# print(index)
+			if drop_row == True:
+				corr_df.drop(index, inplace=True)
+				corr_df.drop(index, axis=1, inplace=True)
+
+		# print(corr_df)
+		# plt.matshow(corr_df)
+		sns.heatmap(corr_df, # display correlation table
+	            xticklabels=corr_df.columns.values,
+	            yticklabels=corr_df.columns.values)
+		plt.subplots_adjust(bottom=0.2,top=0.9, left=0.14)
+		plt.show()
+	else:
+		print('Dataframe is empty')
 
 def scheduler(t=60,n=3,vol_in_btc=2):
 	# t = 10 is the time interval between queries (in seconds)
@@ -202,10 +271,6 @@ def scheduler(t=60,n=3,vol_in_btc=2):
 	except KeyboardInterrupt:
 		print('interrupted!')
 
-def plot():
-	plt.plot([1,2,3],[4,7,4])
-	plt.show()
-
 def get_df_from_csv(path, dataLabels):
 	# check if csv exists, if not creates it then return a handle
 	if(os.path.isfile(path)):
@@ -227,9 +292,42 @@ def write_csv(path, data):
 		for key,value in data.items():
 			csvwriter.writerow({key:value})
 
-def corr_table(first_csv, second_csv):
-	df = pd.read_csv(path)
+def df_init(df):
 
+	# initialize a proper dataframe 
+	df['TimeStamp'] = pd.to_datetime(df['TimeStamp']) # convert to timeStamp
+	df['OpenBuyOrders'] = df['OpenBuyOrders']/100000*2
+	df['OpenSellOrders'] = df['OpenSellOrders']/100000/2
+	df['CorrelationLastBaseVolume'] = df.corr()['Last'].tolist()[1]
+	# df = df[['TimeStamp','Last','OpenBuyOrders','OpenSellOrders','CorrelationLastBaseVolume']]
+	df = df[['TimeStamp','Last','BaseVolume','OpenBuyOrders','OpenSellOrders','CorrelationLastBaseVolume']]
+	df=df.set_index("TimeStamp")
+	# for row in df.iterrows():
+		# index, data = row
+		# print(index,data)
+		# data = data.tolist()
+		# print(data[0],data[1])
+		# print(np.corrcoef(data[0],data[1]))
+		# print(np.corrcoef([0.03,987],[435,987]))
+	# print(df.tail())	
+
+	# print(df.corr())
+	# print(df.corr()['Last'].tolist())
+	# df['Correlation'] = df.corr()['Last']
+	# print(df['Correlation'])
+	# print(df.tail())
+	# df.plot(x='TimeStamp', y=['Last','OpenBuyOrders','OpenSellOrders','Correlation'])
+	# plt.show()
+	# print(df['TimeStamp'])
+	# print(pd.to_datetime(df['TimeStamp']).hour)
+	# print(pd.to_datetime(df['TimeStamp']).dt.time)
+	# print(pd.to_datetime(df['TimeStamp']).dt.minute)
+	# df['datetime'].dt.time
+	# pd.Datetimeindex(df['TimeStamp']).hour
+
+def plot():
+	plt.plot([1,2,3],[4,7,4])
+	plt.show()
 
 if __name__ == '__main__':
 	# new_markets()
@@ -238,10 +336,17 @@ if __name__ == '__main__':
 	# get_ticker()
 	# get_market_volume()
 	# write_to_log('something')
-	scheduler()
+	# scheduler()
+	#
 	# dataLabels = ['TimeStamp','High','Low','Volume','BaseVolume','Bid','Ask','Last','OpenBuyOrders','OpenSellOrders','PrevDay']
 	# get_market_data('BTC-LTC',dataLabels)
-	# corr_table('data/BTC-ETH.csv','data/BTC-DYN.csv')
+
+	# correlation
+	# df = pd.read_csv('data/BTC-ETH.csv')
+	# df_init(df)
+
+	markets_correlation()
+	# plot()
 	# write_to_log()
 	# print(average([1,2,3,4,5],4))
 	# plot()
